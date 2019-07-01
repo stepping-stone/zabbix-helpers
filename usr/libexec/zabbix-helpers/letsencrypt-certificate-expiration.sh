04208ecc98f5938d36c114ce9620c2c8dc0b6a65
Yannick Denzer <yannick.denzer@stepping-stone.ch>
2019-07-01 09:22:08 +0200

    clean-up letsencrypt-certificate-expiration.sh script

--- usr/libexec/zabbix-helpers/letsencrypt-certificate-expiration.sh
+++ usr/libexec/zabbix-helpers/letsencrypt-certificate-expiration.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-SECONDS_UNTIL_NEXT_INVALID=7776000    # 90 days times 86400 seconds per day
-cert_dir="/etc/letsencrypt/live"
- 
-for folder in $(ls ${cert_dir}); do
-  if [ ! -f "${cert_dir}/${folder}/cert.pem" ]; then
-    break
-  fi
 
-  exp_date=$(openssl x509 -noout -enddate -in "${cert_dir}/${folder}/cert.pem" | cut -d= -f 2);
-  t_remaining=$(( ($(date -d "${exp_date}" +%s)-$(date +%s)) ))
- 
-  if [[ ${t_remaining} -lt ${SECONDS_UNTIL_NEXT_INVALID} ]]; then
-    SECONDS_UNTIL_NEXT_INVALID=${t_remaining};
-  fi
+SECONDS_UNTIL_NEXT_INVALID=$((90*24*60*60)) # 90 days
+cert_dir="/etc/letsencrypt/live"
+
+for folder in "$cert_dir"/*
+do
+	if ! test -f "${folder}/cert.pem"
+	then
+		continue
+	fi
+
+	exp_date=$(openssl x509 -noout -enddate -in "${folder}/cert.pem" | cut -d= -f 2)
+	t_remaining=$(( ($(date -d "${exp_date}" +%s)-$(date +%s)) ))
+
+	if test "${t_remaining}" -lt "${SECONDS_UNTIL_NEXT_INVALID}"
+	then
+		SECONDS_UNTIL_NEXT_INVALID="${t_remaining}"
+	fi
 done
 
 echo $(( ${SECONDS_UNTIL_NEXT_INVALID} / 3600 ))
